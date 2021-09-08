0e267ce0e8a64dfa16f5700fe5b23d0dc3c614be
Lisa Cormier <lcormier5@hotmail.com>
2021-09-08 00:38:44 +0000

    added hover logic for tweet area

--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -2,9 +2,40 @@
 $(document).ready(function() {
   console.log('Ready!');
   //event handler for input in tweet-text
-  $(".tweet").on("mouseover", function() {
-    console.log("hovering!")
+  $(".tweet").hover(
+    function() {
     const $tweet = $(this).parents().find(".tweet");
-    $tweet.css("box-shadow", "5px, 10px, pink")
+    $tweet.css('box-shadow', '5px 10px #888');
+  }, 
+    function() {
+    const $tweet = $(this).parents().find(".tweet");
+    $tweet.css('box-shadow', '');
+  })
+  $(".fas.fa-flag").hover(
+    function() {
+    const $flag = $(this).parents().find(".fas.fa-flag");
+    $flag.css('color', 'purple');
+  }, 
+    function() {
+    const $flag = $(this).parents().find(".fas.fa-flag");
+    $flag.css('color', '');
+  })
+  $(".fas.fa-retweet").hover(
+    function() {
+    const $flag = $(this).parents().find(".fas.fa-retweet");
+    $flag.css('color', 'purple');
+  }, 
+    function() {
+    const $flag = $(this).parents().find(".fas.fa-retweet");
+    $flag.css('color', '');
+  })
+  $(".fas.fa-heart").hover(
+    function() {
+    const $flag = $(this).parents().find(".fas.fa-heart");
+    $flag.css('color', 'purple');
+  }, 
+    function() {
+    const $flag = $(this).parents().find(".fas.fa-heart");
+    $flag.css('color', '');
   })
 });
